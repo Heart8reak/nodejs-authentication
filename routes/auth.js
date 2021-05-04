1277c397ce74c313b267cfa0aa9ba8b3f3454fd1
Carlos <c.baez1223@yahoo.com>
2021-05-04 13:57:37 -0400

    completed the jwt token verification

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,10 @@
 const router = require('express').Router()
 const User = require('../model/User')
-const { registerValidation } = require('../validation')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const { registerValidation, loginValidation } = require('../validation')
 
+// REGISTER
 
 router.post('/register', async (req, res) => {
 
@@ -13,22 +16,41 @@ router.post('/register', async (req, res) => {
     const emailExist = await User.findOne({email: req.body.email})
     if(emailExist) return res.status(400).send('Email already exist')
 
+    // HASH PASSWORDS
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(req.body.password, salt)
+
     // CREATE NEW USER
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hashedPassword
     })
     try {
         const savedUser = await user.save()
-        res.send(savedUser)
+        res.send({ message: `Congratulation for registering ${user.name}!`, user: user.id})
     } catch (err) {
         res.status(400).send(err)
-    }
+    } 
 })
 
-// router.post('/login', (req, res) => {
+// LOGIN
 
-// })
+router.post('/login', async (req, res) => {
+    // VALIDATE USER
+    const { error } = loginValidation(req.body)
+    if (error) return res.status(400).send(error.details[0].message)
+    // CHECKING IF EMAIL EXISTS
+    const user = await User.findOne({ email: req.body.email })
+    if (!user) return res.status(400).send('Email is not found!')
+    // CHECK PASSWORD IF CORRECT
+    const validPass = await bcrypt.compare(req.body.password, user.password)
+    if (!validPass) return res.status(400).send('Invalid password')  
+    // CREATE AND ASSIGN WEB TOKEN
+    const token = jwt.sign({ _id: user.id }, process.env.TOKEN_SECRET)
+    res.header('auth-token', token).send(token)
+
+    res.send(`${user.name} is Logged in!`)
+})
 
 module.exports = router
--- validation.js
+++ validation.js
@@ -12,10 +12,10 @@ const registerValidation = data => {
 }
 
 const loginValidation = data => {
-    const schema = {
-        email: Joi.string().min(6).require().email(),
-        password: Joi.string().min(6).require()
-    }
+    const schema = Joi.object({
+        email: Joi.string().min(6).required().email(),
+        password: Joi.string().min(6).required()
+    })
     return schema.validate(data)
 }
 
